fe631152be338d326eb61937deb93ca6c6a0ddd7
ac682 <2419328026@qq.com>
2020-07-13 18:34:52 +0800

    写了半份文档

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # ProjHyperai
-All in one for devs
 
-## Roadmap
-目前框架还在代码框架构建阶段，等出正式版后各项目会改成包引用。
-这意味着现期编译任意项目都需要其依赖项目源码在正确的目录下。
-为此准备了这个仓库，用于一次性创建正确的解决方案已便编译。
+> docs of Hyperai
 
-未来仓库仅提供项目汇总和用于介绍整个项目，兼文档功能。
+## Development
 
-[在这里](https://trello.com/b/sYNnyrnU/projhyperai)跟踪进度。
+```bash
+yarn dev
+yarn build
+```
+
+For more details, please head VuePress's [documentation](https://v1.vuepress.vuejs.org/).
+
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,86 @@
+const resolve = require("vuepress-theme-hope/resolve");
+const { description } = require('../../package')
+
+module.exports = resolve({
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'ProjHyperai Docs',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: "The document for Hyperai",
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    markdown: {
+      enableAll: true,
+    },
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '指南',
+        link: '/guide/0.0.about'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/theGravityLab/ProjHyperai'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+      {
+          title: '接触 ProjHyperai',
+          collapsable: false,
+          children: [
+            '0.0.about.md',
+            '0.1.how-to-read.md',
+            '0.2.contribute.md',
+          ]
+      },
+      {
+          title: '接触 HyperaiShell',
+          collapsable: true,
+          children: [
+            '1.0.about.md',
+            '1.1.deploy.md',
+            '1.2.install-plugins.md'
+          ]
+      },
+      {
+      	title: '开发 HyperaiShell 插件'
+      	collapsable: true,
+      	children: [
+      		'5.0.about.md'
+      	]
+      }
+    ]}
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+});
