8329b5bbad870cb76d005127d9772eca5dfdca9d
Chien Zhang <zqy0224@live.com>
2021-08-06 16:42:25 +0800

    v2021.8.6

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 using Nuke.Common;
@@ -9,29 +8,21 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Compile);
+    [Parameter] readonly string ApiKey;
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [Parameter] readonly string ApiKey;
+
+    [Solution] readonly Solution Solution;
 
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
@@ -84,13 +75,10 @@ class Build : NukeBuild
         .DependsOn(Clean)
         .Executes(() =>
         {
-            foreach (var project in Solution.AllProjects.Where(x=> x.GetOutputType() == "Library"))
-            {
+            foreach (var project in Solution.AllProjects.Where(x => x.GetOutputType() == "Library"))
                 DotNetPack(c => c
                     .SetProcessWorkingDirectory(project.Directory)
                     .SetOutputDirectory(ArtifactsDirectory));
-                
-            }
         });
 
     Target PushPackages => _ => _
@@ -99,12 +87,17 @@ class Build : NukeBuild
         .Executes(() =>
         {
             foreach (var file in Directory.GetFiles(ArtifactsDirectory, "*.nupkg"))
-            {
                 DotNetNuGetPush(c => c
                     .SetSource("https://api.nuget.org/v3/index.json")
                     .SetApiKey(ApiKey)
                     .EnableSkipDuplicate()
                     .SetTargetPath(file));
-            }
         });
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
 }
